294abae21ef743662811c602ed55864bb6f46fcd
JohnathanFlint <nicholas.white142@gmail.com>
2016-09-20 09:55:47 -0600

    Added loops

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,10 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MonsterController 
@@ -23,6 +27,13 @@ public class MonsterController
 //			System.out.println("That's a lot of eyes!!!!!!!!!!! - Scary");
 //		}
 		
+		monsterDecisions();
+		loopMonster();
+	}
+		
+		
+		private void monsterDecisions()
+		{
 		System.out.println("Do you want to change my name?");
 		String answer = keyboardInput.nextLine();
 		
@@ -38,10 +49,15 @@ public class MonsterController
 		}
 		System.out.println(firstMonster);
 		
+		int move = 0;
+		while(move <= 10)
+		{
+			
 		System.out.println("Jacob The Destroyer of Salt attacks you. What do you do?");
 		String firstMove = keyboardInput.nextLine();
 		if (firstMove .equalsIgnoreCase("Run"))
 		{
+			move++;
 			System.out.println("The Destroyer of Salt throws his salt at you. What do you do?");			
 			String firstRunMove = keyboardInput.nextLine();
 			if (firstRunMove .equalsIgnoreCase("Dodge"))
@@ -55,14 +71,60 @@ public class MonsterController
 		}
 		else if(firstMove .equalsIgnoreCase("Fight"))
 		{
+			move++;
 			System.out.println("You rush the monster causing it to roar and swipe at you. What do you do?");
 			String firstFightMove = keyboardInput.nextLine();
 			if (firstFightMove .equalsIgnoreCase("Dodge"))
 			{
+				move++;
 				System.out.println("You narrowly dodge the strike and feel it graze your face leaving a trail of blood. You land next to a flute, sword, and crossbow what do you do?");
+				String secondFightMove = keyboardInput.nextLine();
+				List weaponList = Arrays.asList("grab sword", "grab flute", "grab crossbow");
+				//if (secondFightMove )
+				if(move == 2)
+				{
+					System.out.println("success");
+				}
+				else
+				{
+					System.out.println("failure");
+				}	
+			}
+			}
+				
+		
+		}
+		}
+		
+		
+		
+					
+	
+	private void loopMonster()
+	{
+		for (int food = 0; food <= 10; food++)
+		{
+			System.out.print("HUNGRY!");
+			while(food == 10)
+			{
+				System.out.println("FULL!");
+				food++;
 			}
 		}
+		System.out.println(firstMonster);
+		//define variable for condition/test
+		int numberOfTimes = 0;
+		while(numberOfTimes < 1000)
+		{
+			System.out.print("Firstmonster words" + firstMonster);
 			
+			numberOfTimes++;
+		}
+		
+		for(int count = 0; count < 1000; count++)
+		{
+			System.out.println("First monster words" + firstMonster);
+		}
 	}
 
 }
